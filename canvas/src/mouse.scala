7a6aa1550a4e0d218cb8a79822d8ff5585dc84ac
Christoph Bunte <christophbunte@gmail.com>
2023-12-20 17:06:13 +0100

    wip

--- canvas/src/mouse.scala
+++ canvas/src/mouse.scala
@@ -3,7 +3,9 @@ package mouse
 
 import org.scalajs.dom
 
-def getPos(ev: dom.MouseEvent)(using ctx: dom.CanvasRenderingContext2D): Point =
+private[canvas] def getPos(ev: dom.MouseEvent)(using
+    ctx: dom.CanvasRenderingContext2D
+): Point =
   val bb = ctx.canvas.getBoundingClientRect()
   val x = (ev.clientX - bb.left) * ctx.canvas.width / bb.width
   val y = (ev.clientY - bb.top) * ctx.canvas.height / bb.height
